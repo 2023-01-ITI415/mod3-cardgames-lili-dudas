ea684d4211d0119faaa33f6daaac200b24d55fce
lili-dudas <123129465+lili-dudas@users.noreply.github.com>
2023-03-10 21:38:34 -0500

    deck script

--- Assets/Prospector/__Scripts/CardSpritesSO.cs
+++ Assets/Prospector/__Scripts/CardSpritesSO.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class CardSpritesSO : using UnityEngine;
-
 [CreateAssetMenu(fileName = "CardSpritesSO", menuName = "ScriptableObject/CardSpritesSO")]
 public class CardSpritesSO : ScriptableObject {
     [Header("Card Stock")]
@@ -21,4 +19,61 @@ public class CardSpritesSO : ScriptableObject {
     [Header("Pip Sprites")]
     public Sprite[] faceSprites;
     public Sprite[] rankSprites;
-}
+
+    private static CardSpritesSO S;
+    public static Dictionary <char, Sprite> SUITS { get; private set; }
+
+    public void Init() {
+        INIT_STATICS(this);
+    }
+
+    ///<summary>
+    /// Initiatizes the static elements of CardSpriteSO.
+    /// </summary>
+    /// <paraam name ="cSSO">CardSpriteSO to be assigned to the Singleton S</param>
+    static void INIT_STATICS(CardSpritesSO cSSO) {
+        if (S != null) {
+            Debug.LogError("CardSpriteSO.S can't be set a 2nd time!");
+            return;
+        }
+        S = cSSO; //Initialize the Singleton each time the game starts
+
+        //Initialize the _SUITS Dictionary
+        SUITS = new Dictionary<char, Sprite>() {
+            { 'C', S.suitClub},
+            { 'D', S.suitDiamond},
+            { 'H', S.suitHeart},
+            { 'S', S.suitSpade}
+        };
+    }  
+
+    public static Sprite[] RANKS {
+            get {return S.rankSprites;}
+    }
+
+    ///<summary>
+    /// Searches S.faceSprites for the one with the right name
+    /// </summary>
+    /// <param name="name">The name to search for </param>
+    /// <returns>A face Sprite</returns>
+    public static Sprite GET_FACE(string name){
+        foreach (Sprite spr in S.faceSprites){
+            if (spr.name == name) return spr;
+        }
+        return null;
+    }
+    ///<summary>
+    /// Thus public static property makes S.cardBack accessible to other classes
+    /// </summary>
+    public static Sprite BACK {
+        get { return S.cardBack;}
+    }
+    //<summary>
+    /// Call this to reset the Singleton S to null at the end of a game
+    ///</summary>
+    public static void RESET(){
+            S = null;
+    }
+
+}  
+
---
+++ Assets/Prospector/__Scripts/Deck.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent( typeof(JsonParseDeck))]
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [Header ("Inscribed")]
+    public CardSpriteSO cardSprites;
+    public GameObject prefabCard;
+    public GameObject prefabSprite;
+    public bool startFaceUp = true;
+
+    [Header( "Dynamic")]
+    public Transform deckAnchor;
+    public List<Card> cards;
+
+    private JsonParseDeck jsonDeck;
+
+    static public GameObject SPRITE_PREFAB { get; private set;}
+
+    void Start()
+    {
+        InitDeck();
+    }
+
+    ///<summary>
+    ///The Prospector class will call InitDeck to set up the deck and build
+    ///all 52 card GameObjects from the jsonDeck and the cardSprites information.
+    ///</summary>
+    public void InitDeck() {
+        //create a static refrence to spritePrefab for the Card class to use
+        SPRITE_PREFAB = prefabSprite;
+        //call Init method on the CardSpriteSo instance assigned to cardSprites
+        cardSprites.Init();
+
+        //get a refrence to the JsonParseDeck component
+        jsonDeck = GetComponent<JsonParseDeck>();
+
+        //create anchor for all the Card GameObjects in the Hierarchy
+        if (GameObject.Find( "_Deck") == null) {
+            GameObject anchorGO = new GameObject( "_Deck");
+            deckAnchor = anchorGO.transfrom;
+        }
+
+        MakeCards();
+    }
+
+    ///<summary>
+    /// Create a GameObject for each card in the deck.
+    ///</summary>
+    void MakeCards() {
+        cards = new List<Card>();
+        Card c;
+        
+        //Generate 13 cards for each suit
+        string suits = "CDHS";
+        for ( int i = 0; i < 4; i++) {
+            for ( int j = 1; j <= 13; j++){
+                c = MakeCard( suits[i], j );
+                cards.Add ( c );
+                //This aligns  the cards in nice rows for testing
+                c.transform.position = new Vector3 ((j-7) * 3, (i -1.5f) * 4, 0);
+            }
+        }
+    }
+
+    ///<summary>
+    ///Creates a card GameObject based on suit and rank.
+    ///Note that this method assumes it will be passed a valid suit and rank.
+    ///</summary>
+    ///<param name="suit">The suit of the card (e.g., 'C')</param>
+    ///<param name="rank">The rank from 1 to 13</param>
+    ///<returns></returns>
+    Card MakeCard (char suit, int rank) {
+        GameObject go = Instantiate<GameObject>(prefabCard,deckAnchor);
+
+        Card card = go.GetComponent<Card>();
+
+        //card.Init(suit, rank, startFaceUp);
+
+        return card;
+    }
+
+}
